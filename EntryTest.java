330cc56f63a8969fd4cb6b9ef4ba12ae8861126f
202490 <166980364+202490@users.noreply.github.com>
2024-04-16 13:02:03 +0400

    Update EntryTest.java

--- EntryTest.java
+++ EntryTest.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ *sample entry record for trainess
  */
 package com.stir.cscu9t4practical1;
 
